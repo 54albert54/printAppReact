5ff24e9a100df6e5aa5b1a89a9aeac67936931d5
Alberto Bernecha <126289455+54albert54@users.noreply.github.com>
2024-02-21 22:56:10 +0000

    separate tableview

--- src/components/MainTableView.tsx
+++ src/components/MainTableView.tsx
@@ -1,44 +1,61 @@
 import { useEffect, useState } from "react";
 import CellViews from "./CellViews";
 import Context from "../context/provider";
+import { TDataToShow } from "../context/types";
 
-export default function MainTableView (){
+export default function MainTableView() {
     const context = Context();
-    const [allData , setData ] = useState (context?.data)
+    const [allData, setData] = useState<[TDataToShow]>(context?.data)
 
-    useEffect(()=>{
-        console.log('se agrego uno nuevo');
+    useEffect(() => {
+
         setData(context?.data)
 
-    },[context?.data ])
+    }, [context?.data])
 
-    return(
+    const filter =(filterBy)=>{
+
+        switch (filterBy) {
+            case "id":
+            //as algo
+                
+                break;
         
-            <section className="border-indigo-500 border rounded-md overflow-hidden p-[1px] bg-gray-400 min-h-[30%] max-h-[82%]">
-                <ul className="  w-full flex flex-row justify-between  border shadow-sm font-semibold text-sm bg-white py-1 pl-6 ">
-                   
-                   
-                    <li className="w-[50px] ">{'^'}</li>
-                    <li className="w-[60px] ">ID</li>
-                    <li className="w-[125px] ">Fecha</li>
-                    <li className="w-[150px]  ">Nombre</li>
-                    <li className="w-[120px] relative left-4 "><p>Monto</p></li>
-                    <li className="w-[100px] flex relative right-1  justify-end  ">Activo</li>
-                    <li className="w-[360px] pl-6 ">Motivo</li>
-                    
-                </ul>
-                <div className=" overflow-scroll border-t-2 border-indigo-500  scroll-smooth h-full">
+            default:
+                break;
+        }
+
+    }
+
+    return (
+        <MainTableHeater data={allData} />
+    )
+}
+
+function MainTableHeater({ data }) {
+
+    return (
+        <section className="border-indigo-500 border rounded-md overflow-hidden p-[1px] bg-gray-400 min-h-[30%] max-h-[82%]">
+
+            <ul className="  w-full flex flex-row justify-between  border shadow-sm font-semibold text-sm bg-white py-1 pl-6 ">
+                <li className="w-[50px] ">{'^'}</li>
+                <li className="w-[60px] ">ID</li>
+                <li className="w-[125px] ">Fecha</li>
+                <li className="w-[150px]  ">Nombre</li>
+                <li className="w-[120px] relative left-4 "><p>Monto</p></li>
+                <li className="w-[100px] flex relative right-1  justify-end  ">Activo</li>
+                <li className="w-[360px] pl-6 ">Motivo</li>
+
+            </ul>
+            <div className=" overflow-scroll border-t-2 border-indigo-500  scroll-smooth h-full">
                 {
-                    allData?.map(( data , i)=>(
-                        <CellViews key={i+data.ID} element={data}  idx={i} />
+                    data?.map((data, i) => (
+                        <CellViews key={i + data.ID} element={data} idx={i} />
                     ))
                 }
-                </div>
-                
 
-            </section>
+            </div>
+        </section>
 
-
-        
     )
 }
--- src/context/provider.tsx
+++ src/context/provider.tsx
@@ -2,30 +2,6 @@ import { createContext, useContext, useState } from "react";
 import { referencias } from "./storage";
 import { TDataToShow, TRealvalues, TReference } from "./types";
 
-
-const pruebaCheck:TDataToShow = {
-  Fecha:'12/12/2024' ,
-  FechaColilla:'12/12/2900',
-  CantidadColilla:'123.00',
-  NombreCliente: 'Jon Carter' ,
-  ID:1000,
-  motivo:"mensale para probar esto de preba" ,
-  Cantidad:'123.00' ,
-  active:true,
-  DetalleCantidad: 'mas letras de los numeros'
-};
-const pruebaCheck2:TDataToShow = {
-  Fecha:'12/12/2024' ,
-  FechaColilla:'12/12/2900',
-  CantidadColilla:'123.00',
-  NombreCliente: 'Ana maria ' ,
-  ID:1020,
-  motivo:"mensale para probar esto de preba" ,
-  Cantidad:'123.00' ,
-  active:false,
-  DetalleCantidad: 'mas letras de los numeros'
-};
-
 type TContext = {
   references: [TReference];
   saveReferences: (e) => void;
@@ -42,6 +18,63 @@ type TContext = {
   setPrintArea:(e:boolean)=>void
 };
 
+const pruebaCheck:TDataToShow[] = [{
+  Fecha:'12/12/2024' ,
+  FechaColilla:'12/12/2900',
+  CantidadColilla:'123.00',
+  NombreCliente: 'Jon Carter' ,
+  ID:1000,
+  motivo:"mensale para probar esto de preba" ,
+  Cantidad:'23.00' ,
+  active:false,
+  DetalleCantidad: 'mas letras de los numeros'
+},
+{
+  Fecha:'12/12/2024' ,
+  FechaColilla:'12/12/2900',
+  CantidadColilla:'123.00',
+  NombreCliente: 'Ana maria ' ,
+  ID:1020,
+  motivo:"mensale para probar esto de preba" ,
+  Cantidad:'123.00' ,
+  active:false,
+  DetalleCantidad: 'mas letras de los numeros'
+},
+{
+  Fecha:'11/12/2024' ,
+  FechaColilla:'11/12/2900',
+  CantidadColilla:'123.00',
+  NombreCliente: 'Mike maria ' ,
+  ID:1035,
+  motivo:"mensale para probar esto de preba" ,
+  Cantidad:'99.00' ,
+  active:true,
+  DetalleCantidad: 'mas letras de los numeros'
+},{
+Fecha:'11/12/2024' ,
+FechaColilla:'11/12/2900',
+CantidadColilla:'123.00',
+NombreCliente: 'mario castas' ,
+ID:1033,
+motivo:"mensale para probar esto de preba" ,
+Cantidad:'499.00' ,
+active:true,
+DetalleCantidad: 'mas letras de los numeros'
+},{
+  Fecha:'11/12/2024' ,
+  FechaColilla:'11/12/2900',
+  CantidadColilla:'123.00',
+  NombreCliente: 'josefa' ,
+  ID:1033,
+  motivo:"mensale para probar esto de preba" ,
+  Cantidad:'49.00' ,
+  active:true,
+  DetalleCantidad: 'mas letras de los numeros'
+  },
+]
+
+
+
 
 
 const CheckContext = createContext<TContext | null>(null);
@@ -55,7 +88,7 @@ export const CheckContextProvider = ({ children }) => {
     monto: "",
     clientName: "",
   })
-  const [data ,setData] = useState<TDataToShow[]>([pruebaCheck , pruebaCheck2 ])
+  const [data ,setData] = useState<TDataToShow[]>(pruebaCheck )
 
 
 
